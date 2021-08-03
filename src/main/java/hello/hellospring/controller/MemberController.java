b21ab3dc76c9c3b98f563e96fecf47131cdb6311
joeunhyun <dmsgusdl31@gmail.com>
2021-08-03 23:55:03 +0900

    20210804 자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,6 +1,7 @@
-package hello.hellospring.service;
+package hello.hellospring.controller;
 
 
+import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -12,6 +13,16 @@ public class MemberController {
     @Autowired //컨트롤러와 서비스를 연결
     public MemberController(MemberService memberService){
         this.memberService = memberService;
-    }
+    }// 생성자 주입 - ***********주로 사용
+
+    /*@Autowired private final MemberService memberService;;
+    //필드 주입
+
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }//setter 주입
+*/
+
+
 
 }
